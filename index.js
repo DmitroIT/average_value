20bbe05002389a8cc6411f85a921d68199c79d1f
Dmitro Loshik <dmitroloshik@gmail.com>
2023-04-13 13:10:17 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,15 @@
+﻿let numeric1 = prompt("Введіть перше число");
+numeric1 = Number(numeric1);
+console.log(numeric1);
+
+let numeric2 = prompt("Введіть друге число");
+numeric2 = Number(numeric2);
+console.log(numeric2);
+
+let numeric3 = prompt("Введіть третє число");
+numeric3 = Number(numeric3);
+console.log(numeric3);
+
+let equal = (numeric1 + numeric2 + numeric3) / 3;
+
+alert("Середнє цих чисел буде дорівнювати" + " " + equal);
